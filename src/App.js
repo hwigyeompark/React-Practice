dc10a6582390c9d935017aa84d6111b43400d7b4
hwigyeompark <phg5590@gmail.com>
2018-04-27 14:59:29 +0900

    stopwatch test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import Counter from "./component/Counter";
+/*import Counter from "./component/Counter";*/
+import Stopwatch from "./component/Stopwatch";
 /*import {TodoItem} from './component/TodoList.js';
 import ContactList from "./component/ContactList";*/
 
 class App extends Component {
   render() {
     return (
-        <Counter />
+        <Stopwatch />
     );
   }
 }
---
+++ src/component/Stopwatch.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Stopwatch extends Component{
+    render(){
+        return(
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Stopwatch;

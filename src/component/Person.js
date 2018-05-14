dcfab70a2aa7da8a5197524d506a4a5796322686
hwigyeompark <phg5590@gmail.com>
2018-05-14 13:55:25 +0900

    update Person

--- src/component/Person.js
+++ src/component/Person.js
@@ -4,6 +4,11 @@ class Person extends Component {
     render() {
         return (
             <div>
+                <ul>
+                    <li>{this.props.person.name}</li>
+                    <li>{this.props.person.email}</li>
+                    <li>{this.props.person.phone}</li>
+                </ul>
             </div>
         );
     }
